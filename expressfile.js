4994a311854b610708f0c2e0420560bbffb9c05a
fuyofulo <pheonixdiaz625@gmail.com>
2024-04-03 16:53:36 +0530

    file server

--- expressfile.js
+++ expressfile.js
@@ -2,13 +2,16 @@ const express = require('express');
 const fs = require('fs');
 const path = require('path');
 const app = express();
+const filesDirectory = path.join(__dirname, 'files');
+
 app.listen(3000);
 app.use(express.json());
 
 // Endpoint to get a list of files
 app.get('/files', (req, res) => {
-  fs.readdir('./files', (err, files) => {
+  fs.readdir(filesDirectory, (err, files) => {
     if (err) {
+      console.error('Error reading directory:', err);
       res.status(500).send('Internal Server Error');
     } else {
       res.status(200).json(files);
@@ -19,10 +22,11 @@ app.get('/files', (req, res) => {
 // Endpoint to get the content of a file
 app.get('/file/:filename', (req, res) => {
   const { filename } = req.params;
-  const filePath = `./files/${filename}`;
+  const filePath = path.join(filesDirectory, filename);
 
   fs.readFile(filePath, 'utf8', (err, data) => {
     if (err) {
+      console.error('Error reading file:', err);
       res.status(404).send('File not found');
     } else {
       res.status(200).send(data);
