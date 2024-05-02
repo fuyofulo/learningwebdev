1ce3f8decf4ea7a69964452ca7d582bb1e74004c
fuyofulo <pheonixdiaz625@gmail.com>
2024-05-02 18:10:32 +0530

    prop drilling

---
+++ week-7/prop-drilling/src/App.jsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <Count count={count} setCount={setCount} />
+      
+    </div>
+  );
+}
+
+function Count({ count, setCount }) {
+  return <div>{count}
+  <Buttons count={count} setCount={setCount} /></div>;
+}
+
+function Buttons({ count, setCount }) {
+  return (
+    <div>
+      <button
+        onClick={() => {
+          setCount(count + 1);
+        }}
+      >
+        Increase
+      </button>
+      <button
+        onClick={() => {
+          setCount(count - 1);
+        }}
+      >
+        Decrease
+      </button>
+    </div>
+  );
+}
+
+export default App;
